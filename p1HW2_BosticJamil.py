7900ca98bbe80ac7cad994f21e08497da8695d2f
bosticj <145736823+bosticj@users.noreply.github.com>
2023-09-28 23:11:23 -0400

    p1HW2_BosticJamil.py
    travel expenses

--- p1HW2_BosticJamil.py
+++ p1HW2_BosticJamil.py
@@ -2,12 +2,15 @@
 #9/28/2023
 #CTI-110 P1HW2 - Travel Expense
 #Jamil Bostic
+#Give description of program to user
 print("This program calculates and displays travel expenses")
+#Get price of  expenses from user with descriptive questions
 budget=float(input("Enter budget: "))
 destin=input("Enter your travel destination: ")
 gas=float(input("How much do you think you will spend on gas? "))
 hotel=float(input("Approximately, how much will you need for accomodations/hotel? "))
 food=float(input("Last, how much do you need for food? "))
+#display user input  information 
 print("------------Travel Expenses------------")
 print("Location: ",destin)
 print(f'Initial Budget: {budget:.0f} ')
@@ -17,5 +20,5 @@ print(f'Accomodation: {hotel:.0f} ')
 print(f'Food: {food:.0f} ')
 print('')
 bal=( budget-( hotel + gas + food))
-
+#display remaining balance
 print(f'Remaining Balance: {bal:.0f}')
